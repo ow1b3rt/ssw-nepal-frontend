10bb9b2d6c1ba8723022e79dfe60f89ed7e73e18
ow1b3rt <ow1b3rt>
2026-08-31 16:02:19 +0545

    feat: details page

---
+++ src/app/(public)/services/career-counseling/page.jsx
@@ -0,0 +1,6 @@
+import DetailPage from "@/components/detailPage";
+import { careerCounselingData } from "@/data/careerCounseling";
+
+export default function CareerCounsellingPage() {
+  return <DetailPage data={careerCounselingData} />;
+}
