160bacf14513d1db4a5ff8b2a619752ecf454dc4
SMagar123 <surajpulamim123@gmail.com>
2026-09-02 17:04:36 +0545

    style: fixe image warning for sizes

--- src/components/molecules/ImageContainer.jsx
+++ src/components/molecules/ImageContainer.jsx
@@ -1,13 +1,22 @@
-import Image from 'next/image'
+import Image from "next/image";
 
-export function ImageContainer({ className, ...props }) {
-    return (
-      <div className={className + " overflow-hidden rounded-2xl relative"}>
-        <Image
-          className="hover:scale-105 transition ease-in-out duration-500"
-          fill
-          {...props}
-        />
-      </div>
-    );
+export function ImageContainer({
+  className,
+  sizes = "(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 33vw",
+  alt = "",
+  ...props
+}) {
+  return (
+    <div className={className + " overflow-hidden rounded-2xl relative"}>
+      <Image
+        alt={alt}
+        className="hover:scale-105 transition ease-in-out duration-500"
+        fill
+        sizes={
+          sizes || "(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 33vw"
+        }
+        {...props}
+      />
+    </div>
+  );
 }
