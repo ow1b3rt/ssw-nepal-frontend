6e4919687782b51070be2a11072a809577378498
SMagar123 <surajpulamim123@gmail.com>
2026-09-06 08:49:15 +0545

    style: fix layout for login admin

--- src/packages/admin/components/templates/LoginPage.jsx
+++ src/packages/admin/components/templates/LoginPage.jsx
@@ -36,22 +36,34 @@ export function LoginPage({ loginUrl = "/auth/login", redirectTo = "/admin/dashb
   }
 
   return (
-    <div className="container mx-auto flex h-screen flex-col items-center justify-center gap-4 px-4 lg:flex-row">
-      <ImageContainer src="/logo.png" alt="SSW logo" className="h-f aspect-video w-1/2 md:w-1/3" />
-      <div className="w-full rounded-lg border border-gray-200 bg-white p-4 shadow-[0_10px_30px_rgba(0,0,0,0.15)] md:w-1/2 lg:w-1/3 lg:p-8">
-        <h1 className="mb-6 text-2xl font-semibold">Admin LogIn</h1>
+    <div className="container mx-auto flex h-screen flex-col-reverse items-center justify-center gap-0 px-4 md:flex-row">
+      <div className="border-gray-00 flex w-full flex-col justify-between rounded-lg border bg-white p-4 md:h-96 md:w-1/2 md:rounded-none md:rounded-l-lg lg:w-1/3 lg:p-8">
+        <div className="mb-6 flex flex-col items-center">
+          <h1 className="text-primary-blue-dark text-2xl font-semibold">Welcome Back</h1>
+          <p className="text-text-color font-light">Login to your SSW Admin account</p>
+        </div>
+
         <Form onSubmit={handleSubmit} className="flex flex-col gap-y-4">
-          <Input name="email" type="email" placeholder="Email" required />
+          <Input
+            inputClassName="text-primary-blue"
+            name="email"
+            type="email"
+            placeholder="Email"
+            required
+          />
           <Input name="password" type="password" placeholder="Password" required />
           {error && <p className="text-sm text-red-600">{error}</p>}
           <button
             type="submit"
             disabled={loading}
-            className="bg-primary-green hover:bg-primary-green-dark mt-2 cursor-pointer rounded-md px-4 py-2 text-lg font-medium text-white transition duration-500 ease-in-out disabled:opacity-50"
+            className="bg-primary-green hover:bg-primary-green-dark mt-2 cursor-pointer rounded-md px-4 py-1.5 text-sm font-normal text-white transition duration-500 ease-in-out disabled:opacity-50"
           >
-            {loading ? "Logging in..." : "Log in"}
+            {loading ? "Logging in..." : "Login"}
           </button>
         </Form>
+      </div>
+      <div className="flex w-full flex-col items-center justify-center gap-4 rounded-r-lg border-l-0 p-2 md:h-96 md:w-1/2 md:items-start md:gap-6 md:border lg:w-1/3">
+        <ImageContainer src="/ssw.png" alt="SSW logo" className="aspect-video w-full" />
       </div>
     </div>
   );
