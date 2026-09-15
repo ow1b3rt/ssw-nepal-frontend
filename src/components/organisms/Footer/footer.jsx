f1d577b5b5db9b4ec54bcc7e9a94c943727948bc
SMagar123 <surajpulamim123@gmail.com>
2026-09-15 17:17:51 +0545

    fix: footer issues

--- src/components/organisms/Footer/footer.jsx
+++
@@ -1,7 +0,0 @@
-import FooterView from "@/components/footer"; // your existing markup, renamed
-import { getFooterData } from "@/components/organisms/Footer/footer";
-
-export default async function Footer() {
-  const data = await getFooterData();
-  return <FooterView data={data} />;
-}
